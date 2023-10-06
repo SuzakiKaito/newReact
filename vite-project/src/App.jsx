8f0232078ef4642da3b197d5668a6e3c799d6d59
SuzakiKaito <114970408+SuzakiKaito@users.noreply.github.com>
2023-10-06 16:43:02 +0900

    数増やせた

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
+
 export default function App() {
   // const [count, setCount] = useState(0)
 
+  const [state,setState] = useState(0)
+
+  // const plusButton = () =>{
+  //   setState (state+1)
+  // }
+
+  // const minusbutton = () =>{
+  //   setState (state-1)
+  // }
+
   return (
     <>
-      <div className="flex">
-        <h1>Reactマジでわかりません</h1> 
-        <p>本当に</p>
-      </div>
+      
+        <p>{state}</p>
+        {/* returnの中で変数などを使うときは{}を使う */}
+        <button onClick={() => setState(state + 1)}>plus</button>
+        <button onClick={() => setState(state - 1)}>minus</button>
     </>
   )
 }
